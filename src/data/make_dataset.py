61c30039dad9d0cb905e1d774089c690543ebb28
vamsi1609 <yamuisvamsi@outlook.com>
2021-04-02 12:34:24 +0530

    updated make_dataset.py and updated data\processed folder

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,15 +3,53 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import yaml
+
+
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
+def read_params(config_path):
+    '''
+    takes the config_path as input and 
+    returns the loaded yaml file 
+    '''
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def split_n_save(config_path):
+    '''
+    Function will split the data into train and test set
+    and will save it in data\processed folder
+    '''
+    config = read_params(config_path)
+    # Fetching the configurations
+    train_path = config["split_data"]["train_path"]
+    test_path = config["split_data"]["test_path"]
+    inter_path = config["interim_data"]["source"]
+    test_split = config["split_data"]["test_size"]
+    random_state = config["base"]["random_state"]
+    #Processing and saving
+    print(train_path)
+    print(test_path)
+    df = pd.read_csv(inter_path, sep=",")
+    train, test = train_test_split(df,
+                                   test_size=test_split,
+                                   random_state=random_state)
+    train.to_csv(train_path, sep=",", index=False)
+    test.to_csv(test_path, sep=",", index=False)
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.option('--config', default="params.yaml")
+def main(config):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    split_n_save(config)
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
